c3c29df5889fe646659b0dcf57bf4f753f9d9d5a
Aawni_K <jyesth.dev@outlook.com>
2024-07-25 22:36:25 +0530

    DSA_Queue

---
+++ 01_Javascript/DSA/02_Queue.js
@@ -0,0 +1,62 @@
+// Queue___the data structure which follows FIFO, where the elements are added to the end and removed from the begining.
+// it includes the methods to
+    // -> Add elements
+    // -> Remove elements
+    // -> peek at front element
+    // -> check if queue is empty
+    // -> Gets the size of the queue
+    // -> print the queue elements.
+
+
+class Queue{
+    constructor(){
+        this.queue = []
+    }
+
+    enqueue(data){
+        this.queue.push(data)
+    }
+
+    dequeue(){
+        // Checks if queue is Empty //> EdgeCase
+        if(this.isEmpty()){
+            return "The queue is Empty"
+        }
+        return this.queue.shift()   //_ removes the first element from an array and returns it, simultaneously shifts the elements. here the array is not modified.
+    }
+
+    isEmpty(){
+        return this.queue.length === 0
+    }
+
+    peek(){
+        return this.queue[0]    // gets the first element of an array
+    }
+
+    size(){
+        return this.queue.length
+    }
+
+    printQueue(){
+        let str = ''
+        for (let i = 0; i < this.queue.length; i++) {
+            str += this.queue[i];
+        }
+        return str;
+    }
+
+}
+
+
+// usage
+let myQueue = new Queue()
+myQueue.enqueue(78)
+myQueue.enqueue(78)
+myQueue.enqueue(78)
+myQueue.enqueue(78)
+myQueue.enqueue(78)
+myQueue.dequeue()
+
+console.log(myQueue.printQueue());  // takes the refrence of the constructor
+
+
